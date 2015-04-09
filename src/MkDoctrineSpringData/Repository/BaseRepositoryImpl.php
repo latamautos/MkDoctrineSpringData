863c54ea69e7aa343fc084308ac0fb72a0f726ed
Mapkuff <map.siwapun@gmail.com>
2015-04-10 04:46:02 +0700

    fix dynamic const calling

--- src/MkDoctrineSpringData/Repository/BaseRepositoryImpl.php
+++ src/MkDoctrineSpringData/Repository/BaseRepositoryImpl.php
@@ -243,7 +243,7 @@ abstract class BaseRepositoryImpl implements  BaseRepositoryInterface
 	
 	protected function buildEntityName(){
 	    $clazz = get_called_class();
-	    $namingStrategy = call_user_func("{$clazz}::NAMING_STRATEGY");
+	    $namingStrategy = $clazz::NAMING_STRATEGY;
 	    $clazz = str_replace('\Repository', "\\{$namingStrategy}", $clazz);
 	    $clazz = str_replace('Repository', '', $clazz);
 	    $clazz = str_replace("Impl", '', $clazz);
