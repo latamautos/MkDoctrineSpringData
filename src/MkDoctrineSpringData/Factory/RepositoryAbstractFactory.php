d1e2ef388194465073f3ff13d2f3cb3091ef2370
mapkuff <map.siwapun@gmail.com>
2015-09-20 17:46:07 +0700

    ...

---
+++ src/MkDoctrineSpringData/Factory/RepositoryAbstractFactory.php
@@ -0,0 +1,24 @@
+<?php
+namespace MkDoctrineSpringData\Factory;
+
+use Zend\ServiceManager\AbstractFactoryInterface;
+
+class RepositoryAbstractFactory implements AbstractFactoryInterface
+{
+    
+    public function canCreateServiceWithName(\Zend\ServiceManager\ServiceLocatorInterface $serviceLocator, $name, $requestedName)
+    {
+        
+    }
+
+    public function createServiceWithName(\Zend\ServiceManager\ServiceLocatorInterface $serviceLocator, $name, $requestedName)
+    {
+        
+    }
+
+    
+    
+    
+}
+
+?>
