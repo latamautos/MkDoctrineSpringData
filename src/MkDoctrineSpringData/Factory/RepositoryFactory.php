8f3d394fbe85c8b68fd486bbdb0166e80a8fbbf1
mapkuff <map.siwapun@gmail.com>
2015-10-23 20:25:29 +0700

    ...

--- src/MkDoctrineSpringData/Factory/RepositoryFactory.php
+++ src/MkDoctrineSpringData/Factory/RepositoryFactory.php
@@ -3,6 +3,7 @@ namespace MkDoctrineSpringData\Factory;
 
 use Doctrine\ORM\Repository\RepositoryFactory as DoctrineRepositoryFactoryInterface;
 use MkDoctrineSpringData\Resolver\NamingResolverInterface;
+use Doctrine\ORM\EntityManagerInterface;
 
 class RepositoryFactory implements DoctrineRepositoryFactoryInterface
 {
@@ -15,8 +16,9 @@ class RepositoryFactory implements DoctrineRepositoryFactoryInterface
     
     public function getRepository(\Doctrine\ORM\EntityManagerInterface $entityManager, $entityName)
     {
+        /* @var $entityManager EntityManagerInterface */
         $clazz = $this->namingResolver->resolveRepositoryImplementationName($this->namingResolver->resolveRepositoryInterfaceName($entityName));
-        return new $clazz($entityManager, $entityName);
+        return new $clazz($entityManager, $entityManager->getClassMetadata($entityName));
     }
     
 }
